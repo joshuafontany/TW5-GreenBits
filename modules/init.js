5a9df02c8af38e32658158eafeb4c12517475717
Joshua Fontany <joshua.fontany@gmail.com>
2020-09-23 19:35:32 -0700

    beta

--- modules/init.js
+++ modules/init.js
@@ -75,7 +75,7 @@ exports.startup = async function(callback) {
 							for (let index = 0; index < respArr.length; index++) {
 								var item = respArr[index];
 								var jsonTitle = dataTitle+"/"+item.id;
-								var newTiddler = new $tw.Tiddler({title: jsonTitle, type: "application/json", tags: tagText, name: item.name, product_type_id: item.product_type_id, text:JSON.stringify(item, null, 2)});
+								var newTiddler = new $tw.Tiddler({title: jsonTitle, type: "application/json", tags: tagText, text:JSON.stringify(item, null, 2)});
 								$tw.wiki.addTiddler(newTiddler);
 								//console.log("Added: "+ newTiddler.fields.title);
 							}
@@ -113,14 +113,6 @@ exports.startup = async function(callback) {
 			}
 			var options =  { method: 'get', headers: $tw.greenbits.headers }
 
-			url = $tw.greenbits.url+"/inventory_items?sellable=true"
-			dataTitle = "$:/data/greenbits/location/"+locId+"/inventory_items"
-			await getData(url, options, dataTitle);
-
-			url = $tw.greenbits.url+"/products?by_active=true"
-			dataTitle = "$:/data/greenbits/location/"+locId+"/products"
-			await getData(url, options, dataTitle);
-
 			url = $tw.greenbits.url+"/product_types"
 			dataTitle = "$:/data/greenbits/location/"+locId+"/product_types"
 			await getData(url, options, dataTitle);
@@ -131,6 +123,14 @@ exports.startup = async function(callback) {
 
 			url = $tw.greenbits.url+"/strains"
 			dataTitle = "$:/data/greenbits/location/"+locId+"/strains"
+			await getData(url, options, dataTitle);
+
+			url = $tw.greenbits.url+"/inventory_items?sellable=true"
+			dataTitle = "$:/data/greenbits/location/"+locId+"/inventory_items"
+			await getData(url, options, dataTitle);
+
+			url = $tw.greenbits.url+"/products?by_active=true"
+			dataTitle = "$:/data/greenbits/location/"+locId+"/products"
 			await getData(url, options, dataTitle);
 		}
 
@@ -143,6 +143,34 @@ exports.startup = async function(callback) {
 			for (let index = 0; index < locations.length; index++) {
 				const locId = locations[index];
 				await updateLocation(locId);
+
+				var tiddlers = [], filterText = "[all[tiddlers+shadows]prefix[$:/data/greenbits/location/"+locId+"/products/]]";
+				tiddlers = $tw.wiki.filterTiddlers(filterText);
+				if(tiddlers.length > 0) {
+					//console.log("Products: "+tiddlers.length);
+					for (let index = 0; index < tiddlers.length; index++) {
+						const title = tiddlers[index];
+						var tid = $tw.wiki.getTiddler(title), newTid = {fields:{}};
+						var jsonData = JSON.parse(tid.fields.text);
+						newTid.fields["name"] = jsonData["name"];
+						newTid.fields["tags"] = jsonData["tags"];
+						newTid.fields["sell_price"] = (jsonData["sell_price"] / 100).toFixed(2);
+						newTid.fields["product_type"] = $tw.wiki.getTextReference("$:/data/greenbits/location/"+locId+"/product_types/"+jsonData["product_type_id"]+"##name");
+						newTid.fields["flower_type"] = $tw.wiki.getTextReference("$:/config/greenbits/enums/product/flower_type##"+jsonData["flower_type"]);
+						newTid.fields["concentrate_type"] = $tw.wiki.getTextReference("$:/config/greenbits/enums/product/concentrate_type##"+jsonData["concentrate_type"]);
+						newTid.fields["brand"] = $tw.wiki.getTextReference("$:/data/greenbits/location/"+locId+"/brands/"+jsonData["brand_id"]+"##name");
+						if(jsonData["strain_id"] && typeof(jsonData["strain_id"]) != "null") newTid.fields["strain"] = $tw.wiki.getTextReference("$:/data/greenbits/location/"+locId+"/strains/"+jsonData["strain_id"]+"##name");
+						if ($tw.wiki.getTextReference("$:/data/greenbits/location/"+locId+"/product_types/"+jsonData["product_type_id"]+"##test_results_unit") == 0) {
+							newTid.fields["test_results_thc"] = ((parseFloat(jsonData["test_results_thc"]) || 0) * 100).toFixed(2)+" %";
+							newTid.fields["test_results_cbd"] = ((parseFloat(jsonData["test_results_cbd"]) || 0) * 100).toFixed(2)+" %";	
+						} else if ($tw.wiki.getTextReference("$:/data/greenbits/location/"+locId+"/product_types/"+jsonData["product_type_id"]+"##test_results_unit") == 1) {
+							newTid.fields["test_results_thc"] = (parseFloat(jsonData["test_results_thc"]) || 0).toFixed(2)+" mg";
+							newTid.fields["test_results_cbd"] = (parseFloat(jsonData["test_results_cbd"]) || 0).toFixed(2)+" mg";
+						}
+
+						$tw.wiki.addTiddler(new $tw.Tiddler(tid, newTid.fields, $tw.wiki.getCreationFields()));
+					}
+				}
 				console.log("LocId updated: "+locId);
 			}
 		}
